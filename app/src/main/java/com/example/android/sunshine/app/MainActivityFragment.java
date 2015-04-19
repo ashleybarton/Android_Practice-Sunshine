435ef8f7385b490b580a73dd97bf2ccde8cdfbb8
Ashley Barton <barton.ash@gmail.com>
2015-04-19 13:30:57 -0700

    Testing ListView Functionality with static data

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private ArrayAdapter<String> forecast_adapter;
 
     public MainActivityFragment() {
     }
@@ -18,6 +23,32 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> weatherData = new ArrayList<String>();
+        weatherData.add("Today - Sunny - 88 / 63");
+        weatherData.add("Tomorrow - Sunny - 88 / 63");
+        weatherData.add("Monday - Sunny - 88 / 63");
+        weatherData.add("Tuesday - Sunny - 88 / 63");
+        weatherData.add("Wednesday - Sunny - 88 / 63");
+        weatherData.add("Thursday - Sunny - 88 / 63");
+        weatherData.add("Friday- Sunny - 88 / 63");
+        weatherData.add("Saturday - Sunny - 88 / 63");
+        weatherData.add("Sunday - Sunny - 88 / 63");
+
+        forecast_adapter = new ArrayAdapter<String>(
+                                    //The current context (fragment's parent activity)
+                                    getActivity(),
+                                    //ID of the list item layout
+                                    R.layout.list_item_forecast,
+                                    //links to the specific textView
+                                    R.id.list_item_forecast_textview,
+                                    //reference to our forecast data
+                                    weatherData);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecast_adapter);
+
+        return rootView;
     }
 }
